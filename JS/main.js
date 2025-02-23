4305ea983fe9f9368ef186c935dfd28dc915d8cf
Ayham <ayhamhassouneh@gmail.com>
2025-02-23 20:06:17 +0300

    .

--- JS/main.js
+++ JS/main.js
@@ -53,3 +53,12 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 
+
+
+document.querySelectorAll('.faq-question').forEach(button => {
+    button.addEventListener('click', () => {
+        const answer = button.nextElementSibling;
+        button.classList.toggle('active');
+        answer.classList.toggle('active');
+    });
+});
